75ff6c38deaa138076c739aa8b281678a2471fd1
Hamza <hamzabilalbaig@gmail.com>
2022-12-16 05:40:07 +0500

    need fix

--- index.js
+++ index.js
@@ -9,14 +9,26 @@ const io = new Server();
 app.use(bodyParser.json());
 
 const emailToSocketMapping = new Map();
+const socketToEmailMapping = new Map();
 
 io.on("connection", (socket) => {
   socket.on("join-room", (data) => {
     const { roomId, emailId } = data;
     console.log("User", emailId, "joined room", roomId);
     emailToSocketMapping.set(emailId, socket.id);
+    socketToEmailMapping.set(socket.id, emailId);
     socket.join(roomId);
+    socket.emit("joined-room", { roomId });
     socket.broadcast.to(roomId).emit("user-joined", { emailId });
+  });
+
+  socket.on("call-user", (data) => {
+    const { emailId, offer } = data;
+    const fromEmail = socketToEmailMapping.get(socket.id);
+    const socketId = emailToSocketMapping.get(emailId);
+    socket
+      .to(socketId)
+      .emit("incomming-call", { from: fromEmail, offer: offer });
   });
 });
 
