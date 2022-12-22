e8409f52c6312dc93bebdb2205c157623849fbe3
Waqar Shahid <waqarshahid03@gmail.com>
2022-12-22 07:13:17 +0500

    .

--- index.js
+++ index.js
@@ -13,22 +13,27 @@ const socketToEmailMapping = new Map();
 
 io.on("connection", (socket) => {
   socket.on("join-room", (data) => {
-    const { roomId, emailId } = data;
-    console.log("User", emailId, "joined room", roomId);
+    const { roomId, emailId, name, avatar } = data;
+    console.log("User", name, "joined room", roomId);
     emailToSocketMapping.set(emailId, socket.id);
     socketToEmailMapping.set(socket.id, emailId);
     socket.join(roomId);
-    socket.emit("joined-room", { roomId });
-    socket.broadcast.to(roomId).emit("user-joined", { emailId });
+    socket.emit("joined-room", { roomId, avatar });
   });
 
-  socket.on("call-user", (data) => {
-    const { emailId, offer } = data;
-    const fromEmail = socketToEmailMapping.get(socket.id);
-    const socketId = emailToSocketMapping.get(emailId);
-    socket
-      .to(socketId)
-      .emit("incomming-call", { from: fromEmail, offer: offer });
+  socket.on("sendMessage", ({ name, avatar, text }) => {
+    console.log("sending");
+    io.emit("getMessage", {
+      name,
+      avatar,
+      text,
+    });
+
+    console.log("emote recieved");
+  });
+  socket.on("disconnectUser", (data) => {
+    const { name } = data;
+    console.log("a user disconnected!", name);
   });
 });
 
